90e406637182031b413934deb1882740894a6e82
bidyut420 <76986393+bidyut420@users.noreply.github.com>
2021-01-08 22:26:32 +0530

    Add files via upload

---
+++ a simple login system test1.c
@@ -0,0 +1,15 @@
+// write a simple programe for login system
+#include<stdio.h>
+int main()
+{
+	char name[20],password[20];
+	printf("enter your password\n");
+	scanf("%s",&name);
+	printf("enter your password\n");
+	scanf("%s",&password);
+	printf("\n\n\n\n");
+	printf("this is your name=>%s",name);
+	printf("\n\n");
+	printf("this is your password=>%s",password);
+return 0;
+}
